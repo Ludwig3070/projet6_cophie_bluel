8dcde75342352ad0e076bfdf38f1441b49238530
Ludwig3070 <ludovic.vasseur10@gmail.com>
2024-01-14 20:11:21 +0100

    suite

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -9,29 +9,52 @@ async function fetchWorks() {
     return r.ok ? await r.json() : alert('Serveur introuvable');/* si r.ok return r.json() sino return alert */
 }
 
-{/* affichage de "mes projets" */
-
-let works_fetch = fetchWorks()/* works_fetch est une promesse qui contient le tableau d'objets à traiter */
-    .then(
-        worksArray => {/* worksArray est l'objet (ici un tableau) resultat de la promesse */
-                        worksArray.map((item) => 
-                            {
-                                let gallery = document.querySelector(".gallery")
-                                let figure = document.createElement("figure")
-                                gallery.appendChild(figure)/* ces deux instructions creent les balises figure */
-                                let img = document.createElement("img")
-                                img.src = item.imageUrl
-                                img.alt = item.title
-                                figure.appendChild(img)/* ces trois instructions ajoutent les images  */
-                                let figcaption = document.createElement("figcaption")/* les trois prochaines    instructions ajout figcaption et son texte  */
-                                figcaption.innerText = item.title
-                                figure.appendChild(figcaption)
-                            })
-                    }
-        )
-/* fin d'affichage de "mes projets" */}
 
 
 
 
-console.log(works_fetch)
+
+
+/**
+ * affiche toutes les categories dans la galerie
+ */
+async function displayTous()
+    {   /* affichage de "Tous" */
+        works_fetch.then(
+            worksArray => {/* worksArray est l'objet (ici un tableau) resultat de la promesse */
+                            worksArray.map((item) => 
+                                {
+                                    let gallery = document. querySelector(".gallery")
+                                    let figure = document.  createElement("figure")
+                                    gallery.appendChild (figure)/* ces deux  instructions creent les  balises figure */
+                                    let img = document. createElement("img")
+                                    img.src = item.imageUrl
+                                    img.alt = item.title
+                                    figure.appendChild(img)/* ces trois instructions ajoutent les images  */
+                                    let figcaption =    document.createElement ("figcaption")/* les trois prochaines instructions ajoutent figcaption et son texte  */
+                                    figcaption.innerText =  item.title
+                                    figure.appendChild  (figcaption)
+                                })
+                        }
+            )
+        /* fin de la fonction d'affichage de "TOUS" */}
+/**
+ * fonction qui affiche des boutons avec les differentes categories dans la barre search_categories
+ */
+async function createDisplayCategories()
+    {   let categoriesSet = new Set()
+        works_fetch.then(worksArray =>worksArray.map(item => categoriesSet.add(item.category.name)))
+        categoriesSet.forEach((value)=>console.log(value))
+        /* voir  https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener */
+        
+        
+    }
+
+
+    let works_fetch = fetchWorks()/* works_fetch est une promesse qui contient le tableau d'objets à traiter,elle doit etre appelée en premier pour chercher les données sur le serveur */
+
+
+    displayTous()
+    createDisplayCategories()
+
+
