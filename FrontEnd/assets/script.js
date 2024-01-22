8d6c29b6028618d6322a9fd7f574e73078ebccba
Ludwig3070 <ludovic.vasseur10@gmail.com>
2024-01-22 20:01:37 +0100

    suite fenetre modale commencee

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,17 @@
-/* Ressources  https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener */
+/* some Ressources used */ 
+/* https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener */
 /* https://javascript.info/map-set#set */
+/* https://developer.mozilla.org/fr/docs/Web/API/DOMTokenList/toggle */
+/* https://www.alsacreations.com/actu/lire/1776-element-main-html5.html */
+/* https://www.youtube.com/watch?v=I_UnFEkJsC8 */
+/* https://developer.mozilla.org/fr/docs/Web/API/HTMLElement/hidden */
+/* https://stackoverflow.com/questions/58031311/toggle-text-in-button-onclick */
+/* ressources https://www.youtube.com/watch?v=arjtn3uNiK4 */ /* 16mn */
+/* https://developer.mozilla.org/fr/docs/Web/API/Element/children */
+/* https://developer.mozilla.org/fr/docs/Web/API/Node/childNodes */
+/* https://www.youtube.com/watch?v=PmrHg7q5raw */
+/* https://openclassrooms.com/fr/courses/7697016-creez-des-pages-web-dynamiques-avec-javascript/7911201-sauvegardez-les-donnees-dans-le-localstorage */
+/* https://www.youtube.com/watch?v=3MUmRP9013I */
 
 /* get data from the server */
 
@@ -21,7 +33,7 @@ async function fetchWorks() {
  * put autenthicaton data to get a tocken end a user id
  * @param {string} mail
  * @param {stringtring} password
- * @returns
+ * @returns {promise} 
  */
 async function autenthication_request(mail, password) {
     /* for hidden code */
@@ -60,32 +72,25 @@ async function displayGallery(arg) {
             /* worksArray is the object (here an array) result of the promise */
             let gallery = document.querySelector(".gallery");
             gallery.innerHTML = ""; //erase the gallery before traitment,usefull in others parts of code
-            worksArray.map((item) => {
-                //iteration for each item
+            worksArray.map((item) => {//iteration for each item
+                
                 /**
                  * displays images in the gallery for each item
                  */
                 function display() {
                     //internal function available only here which is used to optimise code
                     let figure = document.createElement("figure");
-                    gallery.appendChild(
-                        figure
-                    ); /* these instructions create the tags figure */
+                    gallery.appendChild(figure); /* these instructions create the tags figure */
                     let img = document.createElement("img"); //create tag img
                     img.src = item.imageUrl; //path of img
                     img.alt = item.title; //alternative text of img
                     figure.appendChild(img); /* these instructions add images in figure */
-                    let figcaption =
-                        document.createElement(
-                            "figcaption"
-                        ); /* the next three instructions add figcaption and its text  */
+                    let figcaption =document.createElement("figcaption"); /* the next three instructions add figcaption and its text  */
                     figcaption.innerText = item.title;
                     figure.appendChild(figcaption);
-                }
-                /* end of function display */
-                if (!arg) {
-                    display();
-                } //select and display the correpondant content of argument
+                }/* end of function display */
+
+                if (!arg) {display();} //select and display the correpondant content of argument
                 else {
                     item.categoryId === arg ? display() : null;
                 }
@@ -94,7 +99,7 @@ async function displayGallery(arg) {
     );
 }
 /**
- * create the search_categrories DOM on index.htm (for main code)
+ * create the search_categrories DOM on index.htm (for main code) (very big function)
  */
 async function displayGallerySearch() {
     works_fetch.then((r) => {
@@ -119,7 +124,7 @@ async function displayGallerySearch() {
 
         /* this second part of code creates the buttons in search_categorie  */
         let gallerySearch = document.querySelector(".search_categories");
-        let i = 1; //usefull to create a data-set after
+        let i = 1; //usefull to create a data-set after (further)
         set.forEach((value) => {
             //creates sequence for each iteration
             let button = document.createElement("button"); //create a button
@@ -135,8 +140,6 @@ async function displayGallerySearch() {
 
         function change(value) {
             let val = Number(value); //transforms value into number
-            /* console.log(val)               //to erase
-                                  console.log(buttons[val])  */
             buttons.forEach((v) => v.classList.remove("button_active")); //remove active class
             buttons[val].classList.add("button_active"); //add active class on the active button
         }
@@ -149,77 +152,113 @@ async function displayGallerySearch() {
     });
 }
 
-/*https://developer.mozilla.org/fr/docs/Web/API/DOMTokenList/toggle */
-/* https://www.alsacreations.com/actu/lire/1776-element-main-html5.html */
-/* https://www.youtube.com/watch?v=I_UnFEkJsC8 */
-/* https://developer.mozilla.org/fr/docs/Web/API/HTMLElement/hidden */
-/* https://stackoverflow.com/questions/58031311/toggle-text-in-button-onclick */
+/**
+ * swap main    main(hidden) <--->main vice versa + include fontweight toggle of login/logout on the header
+ */
+function main_toggle(){
+    const mains = document.querySelectorAll("main"); //it is a nodelist with 2 tags main
+    mains.forEach((main) => main.toggleAttribute("hidden")); //toggles the attribte hidden
+    nav_login_weight_toggle()
+}
+/**
+ * toggle font weight 400<--->600 and vice versa on navlogin/logout
+ */
+function nav_login_weight_toggle(){
+    const nav_login = document.getElementById("nav_login");
+    nav_login.classList.contains("li_logout") ? nav_login.classList.remove("li_logout") : nav_login.classList.add("li_logout")
+}
+
+/**
+ * toggle display of edition mode in the header (opacity 0 <---> 1)
+ */
+function edition_mode_toggle(){
+    const edition_mode = document.querySelector(".edition_mode");
+    const header = document.getElementById("header");
+    edition_mode.classList.contains("none") ? edition_mode.classList.remove("none") : edition_mode.classList.add("none")
+    header.classList.contains("margin_top_100px") ? header.classList.remove("margin_top_100px") : header.classList.add("margin_top_100px")
+}
+/**
+ * move on off and vice versa the display of this button
+ */
+function modify_button_toggle_display(){
+    const modify_button = document.querySelector(".supervisor_access")
+    modify_button.classList.contains("deleted") ? modify_button.classList.remove("deleted") : modify_button.classList.add("deleted")
+}
+
+function nav_log_out_in_toggle(){
+    let nav_login = document.getElementById("nav_login");
+    nav_login.innerText === "logout" ? nav_login.innerHTML = "login" : nav_login.innerHTML = "logout"
+}
+/**
+ * used with event on click on the editor mode only, don't use it somewhere else 
+ */
+function return_to_basic_mode(){
+    const nav_login = document.getElementById("nav_login");
+    modify_button_toggle_display()
+    edition_mode_toggle()
+    nav_log_out_in_toggle()
+    nav_login.removeEventListener("click",return_to_basic_mode)//remove effect on button login/logout 
+    nav_login.addEventListener("click", main_toggle);//manages click on login/logout on the header of the website
+}
+/**
+ * toggle display on search_categories
+ */
+function search_categories_toggle_deleted(){
+    const search_categories = document.querySelector(".search_categories")
+    search_categories.classList.toggle("deleted") 
+}
+
+
 /**
  * this fonction toggles displays of index_section and login_section,it manages click event to
  *
  */
 async function loginListenenerAndToggle() {
     /* hidden code, mode editor */
-    /* this listener manages the login window and opens the edition window after authentification */
+    /* this listener manages the login window and opens the edition window for authentification */
     const nav_login = document.getElementById("nav_login");
-    const mains = document.querySelectorAll("main"); //it is a nodelist with 2 tags main
-    nav_login.addEventListener("click", () => {
-        /* variables */
-        const edition_mode = document.querySelector(".edition_mode"); //it s an hidden tag in header
-        const header = document.getElementById("header");
+    const mains = document.querySelectorAll("main"); //it is a nodelist with 2 tags main    
+    
+
+    nav_login.addEventListener("click", main_toggle);//manages click on login/logout on the header of the website
         
-        /* this first loop manages click on nav login */
-        if (nav_login.innerHTML === "login") {
-            nav_login.innerHTML = "logout"; //toggle login/logout
-            nav_login.classList.add("li_logout"); //toggle css on nav login/logout
-            edition_mode.classList.remove("none"); // remove class none (toggle)
-            header.classList.add("margin_top"); //add margin-top=100px  50px--->100px
-        } else {
-            nav_login.innerHTML = "login"; //toggle login/logout
-            nav_login.classList.remove("li_logout"); //toggle css on nav login/logout
-            edition_mode.classList.add("none"); // add class none (toggle)
-            header.classList.remove("margin_top"); //  remove margin-top=100px-->50px
-        }
-        /* il faut faire qqch ici voir demain */
-        /* this second loop toggles the attribte hidden */
-        mains.forEach((main) => main.toggleAttribute("hidden")); //very good
-    });
-
-    /* ressources https://www.youtube.com/watch?v=arjtn3uNiK4 */ /* 16mn */
-
-    document.forms["editor_form"].addEventListener("submit", function (event) {
-        //manages click on form submit
+    /* manages click on submit for autenthication */
+    document.forms["editor_form"].addEventListener("submit", function (event) {//manages click on form submit "se connecter"
+        
         event.preventDefault();
-        /* console.log(document.forms["editor_form"]["editor_email"]["value"])
-                    console.log(document.forms["editor_form"]["editor_password"]["value"])
-                    console.log(this)
-                    for(let i of this){console.log(i.value)} */
-
-        let server_request = autenthication_request(
+        let server_request = autenthication_request(//call function autenthication_request with 2 parameters inside the form
             document.forms["editor_form"]["editor_email"]["value"],
             document.forms["editor_form"]["editor_password"]["value"]
         );
 
-        server_request.catch((error) => {
+        server_request.catch((error) => {//treatment of error
             /* manages problems of network */
             alert(
                 `${error} \n\n SERVEUR INACCESSIBLE \n VEUILLEZ VERIFIER VOTRE CONNEXION AU RESEAU`
             );
         });
 
-        server_request.then(
+        server_request.then(//treatment of api response
             /* response is an object with userid and token */
             /* in this part of code acces is confirmed so it can manage a new stage */
             (response) => {
-                access_datas = response;
+                access_datas = response;//store userId and token on acces_datas
                 console.log(access_datas);
                 console.log(access_datas.token);
                 console.log(access_datas.userId);
-                /* to return on the other main with editor mode */
-                mains.forEach((main) => main.toggleAttribute("hidden"));
+
+                /* this next code do modifications and return on the website in editor mode */
+                modify_button_toggle_display()//--->move on display of this "button"
+                nav_log_out_in_toggle()// --->login on the website page becomes logout                
+                nav_login.removeEventListener("click",main_toggle)//remove effect on button login/logout               
+                main_toggle()/* to return on the other main with editor mode */               
+                edition_mode_toggle()// move on display of header edition mode
+                search_categories_toggle_deleted()
+                nav_login.addEventListener("click", return_to_basic_mode);//manages click on login/logout on the header of the website
             }
         );
     });
+    
 }
 
 /* main code */
@@ -231,57 +270,5 @@ displayGallerySearch(); //call function to display search buttons,this function 
 displayGallery(); //displays all images
 loginListenenerAndToggle(); //manages login if necessary
 
-/* click management on buttons */
-/* pimple recovery */
-/* voir hicham */
-
-/* https://developer.mozilla.org/fr/docs/Web/API/Element/children */
-
-/* https://developer.mozilla.org/fr/docs/Web/API/Node/childNodes */
-
-/* let sc = document.querySelector(".search_categories")
-  console.log(sc.children[0])
-  console.log(sc.children[1])//undefined
-  console.log(sc)
-  console.log(sc.childNodes)
-  console.log("********************")
-  console.log(sc.childNodes[2])
-  console.log("********************")
-  console.log(sc.children)
-  console.log(sc.children[0].nextElementSibling)//null
-  
-  
- let but = sc.querySelectorAll(".button")
- console.log(but)
- console.log(but.nextSibling)
 
 
- console.log(sc.childNodes.entries())
- arr =Array.from(sc.childNodes.entries)
- console.log(arr)
-
-console.log("li ********************")
-let li = sc.childNodes
-
-console.log(li)
-console.log(li[3])//undefined ?
-console.log(li[2].nextSibling)//undefined ?
-
-li.forEach(v=>console.log(v))//pourquoi pas tous les elements
-console.log('***********************1')
-console.log(sc.innerHTML)
-console.log('***********************2')
-
-let p = sc.children
-console.log(p)
-console.log(p[1]) //undefined ?
-console.log(p[0].nextElementSibling) //null ?
-console.log(p[0].nextSibling.nextElementSibling) //null ? chelou
-
-
-let r = Array.from(p)
-console.log(r)// dingue les elements dynamiques ont disparu
-console.log('**************************************************')
-console.log('**************************************************')
-console.log('**************************************************')
- */
